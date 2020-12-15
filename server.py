1da63f24574bd5e729fc5fa990c70fdff044027b
loayjb96 <50362994+loayjb96@users.noreply.github.com>
2020-12-15 18:08:03 +0200

    first commit

--- server.py
+++ server.py
@@ -7,6 +7,11 @@ app = Flask(__name__, static_url_path='',static_folder='static',template_folder=
 def root():
     return render_template('index.html')
 
-
+@app.route('/register')
+def register():
+    return render_template('register.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 if __name__ == '__main__':
     app.run(port=3000)
