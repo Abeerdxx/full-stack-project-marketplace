c518f781c2d2ac24eb12de8b30944e3efb19834f
loayjb96 <50362994+loayjb96@users.noreply.github.com>
2020-12-16 13:25:12 +0200

    s

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,url_for
 
 app = Flask(__name__, static_url_path='',static_folder='static',template_folder='templates')
-
+import os
 
 @app.route('/')
 def root():
@@ -20,9 +20,22 @@ def do_search():
     email = request.args.get('email')
     phone_number = request.args.get('mobileNo')
     description = request.args.get('comment')
-  
+    
+
    
     return "form_data: {0}, {1}, {2}, {3}".format(full_name, email, phone_number ,description)
 
+
+@app.route('/register', methods=['POST'])
+def upload_file():
+   
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        url = os.path.join('Images/',uploaded_file.filename)
+        a= uploaded_file.save(url)
+        
+        
+
+    return ""
 if __name__ == '__main__':
     app.run(port=3000)
