ab7a04327a5d9c6cf965037a9b9a41437e50293c
loayjb96 <50362994+loayjb96@users.noreply.github.com>
2020-12-16 12:10:34 +0200

    form group update

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__, static_url_path='',static_folder='static',template_folder='templates')
 
@@ -13,5 +13,16 @@ def register():
 @app.route('/about')
 def about():
     return render_template('about.html')
+    
+@app.route("/submit", methods = ['GET'])
+def do_search():
+    full_name = request.args.get('fullName')
+    email = request.args.get('email')
+    phone_number = request.args.get('mobileNo')
+    description = request.args.get('comment')
+  
+   
+    return "form_data: {0}, {1}, {2}, {3}".format(full_name, email, phone_number ,description)
+
 if __name__ == '__main__':
     app.run(port=3000)
